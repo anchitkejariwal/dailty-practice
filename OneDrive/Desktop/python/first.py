b4bd14aef22d1a75b289c85258f0579d561c7a90
Anchit <anchitkejariwal@gmail.com>
2026-06-16 20:19:26 +0530

    Add practice python files

---
+++ OneDrive/Desktop/python/first.py
@@ -0,0 +1,7 @@
+print( "hello world")
+
+print("yo yo")
+"""
+hello mein pehli baar try kar    rhe hain python ko
+yeh ek bahut hi accha language hai  
+"""
---
+++ OneDrive/Desktop/python/hello.py
@@ -0,0 +1,8 @@
+def hello(to="world"):
+    print("hello " + to)
+
+def main():
+    name=input("what is your name?")
+    hello(name)
+
+main()
